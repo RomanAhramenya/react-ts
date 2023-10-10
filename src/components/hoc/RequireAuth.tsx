67b0877306b3872c0e4f14cb2f08f56187fccf23
Roman <romanahramenya@gmail.com>
2023-10-10 22:15:36 +0300

    auth

---
+++ src/components/hoc/RequireAuth.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import { useLocation, Navigate } from 'react-router-dom'
+interface IRequireAuthProps {
+    children: ReactNode
+}
+
+const RequireAuth = (props: IRequireAuthProps) => {
+    const { children } = props
+    const auth = false
+    const navigate = useLocation()
+
+    if (!auth) return <Navigate to='/login' state={{ from: location }} />
+    return children
+
+
+
+
+}
+
+export default RequireAuth
